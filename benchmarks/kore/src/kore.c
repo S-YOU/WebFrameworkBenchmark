805168fb597ee87273dcf87b4ee23be09f8dbb2d
S-YOU <S-YOU@users.noreply.github.com>
2016-02-20 03:12:47 +0000

    custom

--- benchmarks/kore/src/kore.c
+++ benchmarks/kore/src/kore.c
@@ -1,11 +1,10 @@
 #include <kore/kore.h>
 #include <kore/http.h>
 
-int		page(struct http_request *);
+int	page(struct http_request *);
 
-int
-page(struct http_request *req)
-{
-	http_response(req, 200, "Hello World", 12);
+int page(struct http_request *req) {
+	http_response_header(req, "content-type", "text/html");
+	http_response(req, 200, "Hello World!", 13);
 	return (KORE_RESULT_OK);
 }
